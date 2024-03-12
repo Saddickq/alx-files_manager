ea3354b84c0783366b90d247b4616089aae5e177
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-03-12 22:41:04 +0000

    saddiq db

--- utils/db.js
+++ utils/db.js
@@ -2,17 +2,16 @@ import { MongoClient } from "mongodb";
 
 class DBClient {
     constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
+        this.host = process.env.DB_HOST || 'localhost';
+        this.port = process.env.DB_PORT || 27017;
+        this.database = process.env.DB_DATABASE || 'files_manage';
+        this.uri = `mongodb://${this.host}:${this.port}`;
         
-        const uri = `mongodb://${this.host}:${this.port}`;
-        
-        MongoClient.connect(uri, { useUnifiedTopology: true }, (error, client) => {
+        MongoClient.connect(this.uri, { useUnifiedTopology: true }, (error, client) => {
             if (error) this.dbClient = false;
             else this.dbClient = client.db(database);
         });
-        
+    
     };
 
     isAlive() {
